ec2197549426fe4980356340671a02e82662be5c
Jamie Goodnight <jamiegoodnight@Jamies-Air.attlocal.net>
2020-01-07 15:21:13 -0500

    :construction: Building traversal

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import requests
+import json 
 
 jamie_api_key= 'Token 6b056b046e07f644c8a86d42fb659a70821439c2'
 
@@ -12,9 +13,28 @@ class Player:
         self.name = name
         self.currentRoom = startingRoom
     def travel(self, direction):
-        print("travel")
+        print("Direction inputed:", direction)
+        if direction == "n":
+            data={"direction": "n"}
+        if direction == "s":
+            data={"direction": "s"}
+        if direction == "e":
+            data={"direction": "e"}
+        if direction == "w":
+            data={"direction": "w"}
+        res=requests.post(
+            'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=json.dumps(data)
+        )
+
+        nextRoom=json.loads(res.text)
+        self.currentRoom=nextRoom
+        print(nextRoom, "Here is our new room")
+
     def init(self):
         res=requests.get(
             'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', headers=headers
         )
-        print(res.text, "Here is our response")
+        nextRoom=json.loads(res.text)
+        self.currentRoom=nextRoom
+        print(nextRoom, "Here is our response")
+        
